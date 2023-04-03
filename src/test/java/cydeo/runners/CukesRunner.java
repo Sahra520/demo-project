7443962d0712b4f5e18cf69c0a4d861d2878c8c1
KMN <kuwwatt@gmail.com>
2023-04-03 14:56:57 +0200

    GoogleSearchPage.java

---
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -0,0 +1,27 @@
+package cydeo.runners;
+
+import cydeo.utilities.Driver;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber_reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+
+        tags = "@wip" , //"@employee and not @admin"----like this
+        publish = true
+)
+
+public class CukesRunner {
+
+}
+
+
